50e57395fc3514d00b490d126640b15947b79a43
capitalwang1014 <zy.wang1014@gmail.com>
2021-02-09 22:15:09 -0500

    Base class file upload

---
+++ Rental.py
@@ -0,0 +1,21 @@
+'''
+Author: Ziyang Wang
+This project will build a system for data collection/refreshing, analysis/modeling , monitoring/alerting of a rental real estate investment.
+
+'''
+
+class RData:
+    def __init__(self):
+        pass
+
+class GeoPross:
+    def __init__(self):
+        pass
+
+class TimePross:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    #add
+    pass
